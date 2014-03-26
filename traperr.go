26e550aa9a3b4aab189657d5a61316460baf66cc
Glyn Normington <gnormington@gopivotal.com>
2014-03-26 17:38:44 +0000

    Capture stack trace

--- traperr.go
+++ traperr.go
@@ -1,19 +1,41 @@
 package traperr
 
+import (
+	"runtime"
+)
+
 type Err interface {
+
+	// Returns the error message.
 	Error() string
+
+	// Returns the stack trace of the error.
+	StackTrace() string
 }
 
-// Returns an err containing the given message.
+// Returns an Err containing the given message.
 func New(message string) Err {
-	return &errImpl{message}
+	var stack [4096]byte
+
+	runtime.Stack(stack[:], false)
+
+	pc, file, line, _ := runtime.Caller(1)
+
+	return &errImpl{message, stack[:], pc, file, line}
 }
 
 type errImpl struct {
 	message string
+	stackTrace []byte
+	pc uintptr
+	file string
+	line int
 }
 
-// Returns the message associated with the err.
 func (e *errImpl) Error() string {
 	return e.message
+}
+
+func (e *errImpl) StackTrace() string {
+	return string(e.stackTrace)
 }
--- traperr_test.go
+++ traperr_test.go
@@ -2,17 +2,32 @@ package traperr_test
 
 import (
 	"github.com/glyn/traperr"
+	"strings"
 	"testing"
 )
 
+const testMessage = "test"
+
 func TestMessageCapture(t *testing.T) {
 
-	e := traperr.New("test")
+	e := traperr.New(testMessage)
 
 	actual := e.Error()
 
-	if actual != "test" {
-		t.Errorf("%q != %q", actual, "test")
+	if actual != testMessage {
+		t.Errorf("%q != %q", actual, testMessage)
 	}
+}
 
+func TestStackTraceCapture(t *testing.T) {
+
+	const stackPortion = "traperr_test.TestStackTraceCapture"
+
+	e := traperr.New(testMessage)
+
+	stack := e.StackTrace()
+
+	if !strings.Contains(stack, stackPortion) {
+		t.Errorf("%q does not contain %q", stack, stackPortion)
+	}
 }
